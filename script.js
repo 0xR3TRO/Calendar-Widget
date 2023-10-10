73a2e25241b81d84dd78c3b8fc592a1d5c989427
0xR3TR0 <oliwier@klefas.com>
2023-10-10 22:33:12 +0200

    JavaScript (v.1.0)

---
+++ script.js
@@ -0,0 +1,14 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date(); // Poprawiona nazwa konstruktora
+
+const weekDays = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+const allMonths = ["January", "February","March","April","May","June","July","August","September","October","November","December"];
+
+date.textContent = today.getDate().toString();
+day.textContent = weekDays[today.getDay()];
+month.textContent = allMonths[today.getMonth().toString()]; // Dodaj 1, ponieważ styczeń to 0, luty to 1, itd.
+year.textContent = today.getFullYear().toString();
